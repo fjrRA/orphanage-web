11e2229baacba09dad76f969541e902150bed7b1
Jarmyko <jarmyko98@gmail.com>
2024-09-16 21:15:09 +0700

    menghapus package.json dan komen pada index.js

--- app/index.js
+++ app/index.js
@@ -1,14 +1,11 @@
-// Select hamburger button and main navigation
 const hamburgerButton = document.getElementById('hamburger-button');
 const mainNav = document.getElementById('main-nav');
 
-// Toggle menu visibility when the hamburger button is clicked
+
 hamburgerButton.addEventListener('click', () => {
-  // Toggle aria-expanded attribute
   const isExpanded = hamburgerButton.getAttribute('aria-expanded') === 'true';
   hamburgerButton.setAttribute('aria-expanded', !isExpanded);
 
-  // Toggle visibility of the main navigation
   if (mainNav.classList.contains('hidden')) {
     mainNav.classList.remove('hidden');
   } else {
