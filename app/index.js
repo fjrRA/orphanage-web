b8aa3651af0ce9acbde23463023deb7245d97f74
Jarmyko <jarmyko98@gmail.com>
2024-09-17 05:44:52 +0700

    Navbar mode mobile telah berfungsi

--- app/index.js
+++ app/index.js
@@ -1,14 +1,26 @@
 const hamburgerButton = document.getElementById('hamburger-button');
 const mainNav = document.getElementById('main-nav');
+const dropdowns = document.querySelectorAll('.dropdown');
 
+dropdowns.forEach(dropdown => {
+  const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+
+  dropdown.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    document.querySelectorAll('.dropdown-menu.active').forEach(activeMenu => {
+      if (activeMenu !== dropdownMenu) {
+        activeMenu.classList.remove('active');
+      }
+    });
+
+    dropdownMenu.classList.toggle('active');
+  });
+});
 
 hamburgerButton.addEventListener('click', () => {
   const isExpanded = hamburgerButton.getAttribute('aria-expanded') === 'true';
   hamburgerButton.setAttribute('aria-expanded', !isExpanded);
 
-  if (mainNav.classList.contains('hidden')) {
-    mainNav.classList.remove('hidden');
-  } else {
-    mainNav.classList.add('hidden');
-  }
+  mainNav.classList.toggle('hidden');
 });
