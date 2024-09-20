0cd9133a4522d7723268bca09174fff125b97499
Jarmyko <jarmyko98@gmail.com>
2024-09-20 21:42:45 +0700

    menambahkan halaman-halaman, men-styling header dan footer disetiap halaman, menghapus dropdown pada index.js.

--- app/index.js
+++ app/index.js
@@ -1,22 +1,5 @@
 const hamburgerButton = document.getElementById('hamburger-button');
 const mainNav = document.getElementById('main-nav');
-const dropdowns = document.querySelectorAll('.dropdown');
-
-dropdowns.forEach(dropdown => {
-  const dropdownMenu = dropdown.querySelector('.dropdown-menu');
-
-  dropdown.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    document.querySelectorAll('.dropdown-menu.active').forEach(activeMenu => {
-      if (activeMenu !== dropdownMenu) {
-        activeMenu.classList.remove('active');
-      }
-    });
-
-    dropdownMenu.classList.toggle('active');
-  });
-});
 
 hamburgerButton.addEventListener('click', () => {
   const isExpanded = hamburgerButton.getAttribute('aria-expanded') === 'true';
