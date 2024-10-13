166d82ecdae40e32668e0bcbbca1b75fd9cc6c02
Jarmyko <jarmyko98@gmail.com>
2024-10-13 21:02:07 +0700

    Mengisi konten pada halaman fasilitas.

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+// Responsive Navbar
 const hamburgerButton = document.getElementById('hamburger-button');
 const mainNav = document.getElementById('main-nav');
 
@@ -8,6 +9,7 @@ hamburgerButton.addEventListener('click', () => {
   mainNav.classList.toggle('hidden');
 });
 
+// Fecth & Showing Program Pages
 function fetchProgramData() {
   fetch('../DATA/program__pages.json')
     .then(response => response.json())
@@ -33,4 +35,32 @@ function displayProgramData(programRutinitas) {
   });
 }
 
+// Fecth & Showing Facility Pages
+function fetchFacilityData() {
+  fetch('../DATA/facility__pages.json')
+    .then(response => response.json())
+    .then(data => displayFacilityData(data.programRutinitas))
+    .catch(error => console.error('Error fetching facility data:', error));
+}
+
+function displayFacilityData(facilities) {
+  const facilityContainer = document.querySelector('section.facility');
+
+  facilityContainer.innerHTML = '';
+
+  facilities.forEach(facility => {
+    const facilityCard = document.createElement('div');
+    facilityCard.classList.add('facility__container');
+
+    facilityCard.innerHTML = `
+      <img src="${facility.gambarFasilitas}" alt="${facility.judulFasilitas}">
+      <h3>${facility.judulFasilitas}</h3>
+      <p>${facility.deskripsiFasilitas}</p>
+    `;
+
+    facilityContainer.appendChild(facilityCard);
+  });
+}
+
 document.addEventListener('DOMContentLoaded', fetchProgramData);
+document.addEventListener('DOMContentLoaded', fetchFacilityData);
